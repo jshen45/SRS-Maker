14c0b17e64725f980e366db08e088af7b1f784de
vical1024 <vical1024@gmail.com>
2015-10-02 14:40:24 +0900

    20151002 Office

--- SRS Maker/View/IO.xaml.cs
+++ SRS Maker/View/IO.xaml.cs
@@ -24,5 +24,10 @@ namespace SRS_Maker.View
         {
             InitializeComponent();
         }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
